6b696e25eaea55896aa63e87f68143dff8512477
Joe Viviano <Joe Viviano>
2016-06-04 11:09:31 -0400

    initial upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You do not need to add the <script> element to a javascript file
+/* Use this format for multilple
+line comments in javascript */
+function ButtonFunction() {
+  alert("For help with this website, please contact help@mmouse.com");
+}
